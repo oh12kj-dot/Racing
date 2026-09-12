1e9e0980cb43a7f6862c4f329df96c3c46178931
oh12kj-dot <oh12kj@gmail.com>
2026-09-12 23:47:36 +0900

    Add race class setting

--- iphone-demo/v10-settings.js
+++ iphone-demo/v10-settings.js
@@ -1,6 +1,6 @@
 export const DEFAULT_SETTINGS={
   circuit:'CHAMPIONSHIP',laps:6,ai:1,weather:'DYNAMIC',time:14.2,
-  failures:1,tyreWear:1,safetyCar:true,sound:true
+  failures:1,tyreWear:1,safetyCar:true,sound:true,raceClass:'MIXED'
 };
 const KEY='racing_v10_settings';
 export function loadSettings(){
