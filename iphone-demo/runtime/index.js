94a331aa8336a824db16ef05c3a66db09b00edaa
oh12kj-dot <oh12kj@gmail.com>
2026-09-19 01:38:17 +0900

    Route runtime through rebuilt road surface

--- iphone-demo/runtime/index.js
+++ iphone-demo/runtime/index.js
@@ -1,4 +1,4 @@
-export {buildWorld} from './world-trajectory-lines.js';
+export {buildWorld} from './world-road-rebuild.js';
 export {createRace} from './race-performance.js';
 export {createAudio} from './audio-race-presence.js';
 export {createUI} from './ui-performance.js';
