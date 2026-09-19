9d8056c8496e0786ec3d4385cd3377977db0863c
oh12kj-dot <oh12kj@gmail.com>
2026-09-19 19:17:13 +0900

    refactor(ui): remove obsolete spectator UI wrapper

--- iphone-demo/runtime/ui-spectator-runtime.js
+++
@@ -1,6 +0,0 @@
-import {createUI as createStableUI} from './ui.js';
-import {attachSpectatorInsights} from './ui-spectator-insights.js';
-
-export function createUI(W,R,D,E,C,A,settings,saveSettings){
-  return attachSpectatorInsights(createStableUI(W,R,D,E,C,A,settings,saveSettings),R,D);
-}
